5c945aff2397050a643d3010ddf003ec8431fa1a
ProfP30 <p.salajean@gmx.de>
2021-10-28 14:27:48 +0200

    Make aliases distro-agnostic.

--- my_functions.zsh
+++ my_functions.zsh
@@ -1,3 +1,15 @@
+#!/bin/bash
+logout()
+{
+    case $DESKTOP_SESSION in
+       xfce) xfce4-session-logout --logout                     ;;
+       mate) mate-session-save --logout-dialog                 ;;
+       lxqt) lxqt-leave --logout                               ;;
+       *plasma*) qdbus org.kde.ksmserver /KSMServer logout 0 3 3 ;;
+
+       *) echo "Sorry, Desktop Environment is uknown (no xfce/mate/lxqt/plasma)!" ;;
+    esac
+}
  
 wmv2mp4() {
     ffmpeg -i "$1".wmv -c:v libx264 -crf 23 "$1".mp4
@@ -10,4 +22,30 @@ wtfis() {
 
 clear_hist() {
 for number in $(history | grep "$1" | sed 's/\|/ /'|awk '{print $1}' | sort -nr); do history -d "${number}"; done
+}
+
+# # ex = EXtractor for all kinds of archives
+# # usage: ex <file>
+ex ()
+{
+  if [ -f $1 ] ; then
+    case $1 in
+      *.tar.bz2)   tar xjf $1   ;;
+      *.tar.gz)    tar xzf $1   ;;
+      *.bz2)       bunzip2 $1   ;;
+      *.rar)       unrar x $1   ;;
+      *.gz)        gunzip $1    ;;
+      *.tar)       tar xf $1    ;;
+      *.tbz2)      tar xjf $1   ;;
+      *.tgz)       tar xzf $1   ;;
+      *.zip)       unzip $1     ;;
+      *.Z)         uncompress $1;;
+      *.7z)        7z x $1      ;;
+      *.deb)       ar x $1      ;;
+      *.tar.xz)    tar xf $1    ;;
+      *)           echo "'$1' cannot be extracted via ex()" ;;
+    esac
+  else
+    echo "'$1' is not a valid file"
+  fi
 }
